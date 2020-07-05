0144f3c3a0af3910de35f3df8d1a99337a703411
jonathanvdc <jonathan.vdc@outlook.com>
2020-07-05 14:41:03 +0200

    Remap .interp section to avoid conflicts

--- src/cere/lel.py
+++ src/cere/lel.py
@@ -67,9 +67,9 @@ def create_user_main(mode_opt,LOOP):
 
 def compile_memory_dump_objects(mode_opt, DIR):
     if not mode_opt.static:
-        DUMP_ARGS="-Wl,--section-start=.text=0x60004000 -Wl,--section-start=.init=0x60000000"
+        DUMP_ARGS="-Wl,--section-start=.text=0x60004000 -Wl,--section-start=.init=0x60000000 -Wl,--section-start=.interp=0x80000000"
     else:
-        DUMP_ARGS="-Wl,--section-start=.text=0x6004000 -Wl,--section-start=.init=0x6000000"
+        DUMP_ARGS="-Wl,--section-start=.text=0x6004000 -Wl,--section-start=.init=0x6000000 -Wl,--section-start=.interp=0x80000000"
     safe_system("rm -f {0}/objs.S".format(DIR))
     OBJ=open("{0}/objs.S".format(DIR),'w')
     for FILE in os.listdir(DIR):
